656ee4f81818c76dbc8ab7750cd47526cb358fa1
LucasFr <lucasfariasrodrigues25@gmail.com>
2021-01-10 12:17:17 -0300

    Adiciona localStorage para armazenar os repositorios

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable camelcase */
-import React, { useState, FormEvent } from 'react';
+import React, { useState, FormEvent, useEffect } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import api from '../../services/api';
 
@@ -18,7 +18,23 @@ interface Repository {
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
   const [inputError, setInputError] = useState('');
-  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepository = localStorage.getItem(
+      '@GithubExplorer:repositories',
+    );
+
+    if (storagedRepository) {
+      return JSON.parse(storagedRepository);
+    }
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
 
   async function handleFormSubmit(
     event: FormEvent<HTMLFormElement>,
